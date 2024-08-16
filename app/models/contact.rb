cff0ac425866636f013af71815e173fffdde361f
Haya B <lacourt.christelle@gmail.com>
2024-08-16 11:51:48 +0100

    setting up mailgun

--- app/models/contact.rb
+++
@@ -1,9 +0,0 @@
-class Contact
-  include ActiveModel::Model
-  include ActiveModel::Validations
-
-  attr_accessor :name, :email, :message
-
-  validates :name, :email, :message, presence: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-end

6be3258f917d23372c08ad05771e9365d061ae52
Haya B <lacourt.christelle@gmail.com>
2024-08-16 12:03:06 +0100

    configuring my contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,10 @@
 class ContactMailer < ApplicationMailer
+  def contact_email(contact)
+    @contact = contact
+    mail(
+      from: @contact.email,
+      to: ENV['CONTACT_EMAIL'],
+      subject: 'New Contact Form Message'
+    )
+  end
 end

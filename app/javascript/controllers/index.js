d463707b4e9d00db8d6491783270ddc53270c1cb
Haya B <lacourt.christelle@gmail.com>
2024-08-19 22:11:22 +0100

    contact mailer added and js confirgured for alert messages

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,14 @@
 // Import and register all your controllers from the importmap under controllers/*
-
-import { application } from "controllers/application"
+import { application } from "controllers/application";
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+eagerLoadControllersFrom("controllers", application);
+
+// Import and register the custom AlertsController
+import AlertsController from "controllers/alerts_controller"; // Ensure this path matches where your file is located
+
+application.register("alerts", AlertsController);
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"

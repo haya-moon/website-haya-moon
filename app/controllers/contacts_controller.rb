29d49eb7ad0362e860255475027564b46d742556
Haya B <lacourt.christelle@gmail.com>
2024-08-21 11:38:26 +0100

    contact controller added api configuration and commented and contact mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,29 @@
 class ContactsController < ApplicationController
   def create
-    @contact = Contact.new(contact_params)
-    if @contact.valid?
-      ContactMailer.contact_email(@contact).deliver_now
-      flash[:success] = 'Your message has been sent!'
-      redirect_to root_path
+    # Directly use form parameters without the need for a Contact model
+    name = params[:contact][:name]
+    email = params[:contact][:email]
+    message = params[:contact][:message]
+
+    if name.present? && email.present? && message.present?
+      # Use Mailgun API directly
+      begin
+        RestClient.post "https://api:#{ENV['MAILGUN_API_KEY']}@api.mailgun.net/v3/#{ENV['MAILGUN_DOMAIN']}/messages",
+                        from: email,
+                        to: ENV['CONTACT_EMAIL'],
+                        subject: "New Contact Form Message from #{name}",
+                        text: message
+
+        flash[:success] = 'Your message has been sent!'
+        redirect_to root_path
+      rescue RestClient::ExceptionWithResponse => e
+        Rails.logger.error "Mailgun API error: #{e.response}"
+        flash[:error] = 'Error occurred, your message has not been sent.'
+        render 'pages/home'
+      end
     else
-      flash[:error] = 'Error occurred, your message has not been sent.'
+      flash[:error] = 'Please fill in all the required fields.'
       render 'pages/home'
     end
-  end
-
-  private
-
-  def contact_params
-    params.require(:contact).permit(:name, :email, :message)
   end
 end
